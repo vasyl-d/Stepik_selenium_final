845436707d8a5a79033d21c5826280a908e4a192
Vasyl-D <vpets07@gmail.com>
2022-06-14 13:41:00 +0300

    PageObject 1st stage

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage 
 
 
 class TestMainPage1():
 
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        browser.get(link)
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        page = MainPage(browser, link)
+        page.open()                      # открываем страницу
+        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
