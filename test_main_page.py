3f8e86d3e5eac105a08bb940ff3d2e73c36b7f82
Vasyl-D <vpets07@gmail.com>
2022-06-15 10:47:55 +0300

    add except for errors

--- test_main_page.py
+++ test_main_page.py
@@ -4,8 +4,15 @@ from .pages.main_page import MainPage
 
 class TestMainPage1():
 
-    def test_guest_can_go_to_login_page(self, browser):
+#    def test_guest_can_go_to_login_page(self, browser):
+#        link = "http://selenium1py.pythonanywhere.com/"
+#        page = MainPage(browser, link)
+#        page.open()                      # открываем страницу
+#        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+    def test_guest_should_see_login_link(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
         page = MainPage(browser, link)
-        page.open()                      # открываем страницу
-        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+        page.open()
+        page.should_be_login_link()
+        print(page.error_msg)
